e901bf7b94b215d4c3ba5f47ee0d1f8408f140ca
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-04-21 10:51:27 +0200

    Add dark mode functionality
    Added the dark mode functionality.
    Theme can now be changed from light to dark.
    To do it we use button with moon or sun.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';;
 import { Todo } from './types';
+import { FiSun, FiMoon } from 'react-icons/fi';
 import './App.css';
 
 const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(() => {
+    const stored = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    return stored ? stored === 'dark' : prefersDark;
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  }, [darkMode]);
+
+  const toggleTheme = () => setDarkMode((prev) => !prev);
+
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = (text: string) => {
@@ -26,6 +46,10 @@ const App: React.FC = () => {
 
   return (
     <div className='app-container'>
+      <button onClick={toggleTheme} className='theme-toggle-btn'>
+        {darkMode ? <FiSun size={24} /> : <FiMoon size={24} />}
+      </button>
+      
       <h1>Tasks List</h1>
       <TodoInput addTodo={addTodo} />
       <TodoList todos={todos} toggleComplete={toggleComplete} />
